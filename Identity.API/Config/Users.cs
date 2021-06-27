538e0745226441854f864b01e8ee6798e630cfd8
Adedamola Adeyemi <adedamola.adeyemi@fcmb.com>
2021-06-27 08:17:22 +0100

    Add project files.

---
+++ Identity.API/Config/Users.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity.API.Config
+{
+    public static class Users
+    {
+        public static List<TestUser> GetTestUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId="subject-id",
+                    Username ="user",
+                    Password ="password",
+                    Claims =new List<Claim>
+                    { 
+                        new Claim(JwtClaimTypes.Email,"user@user.com"),
+                        new Claim(JwtClaimTypes.Role,"admin")
+                    }
+
+                }
+            };
+        }
+    }
+}
---
+++ Identity.API/Startup.cs
@@ -0,0 +1,66 @@
+using Identity.API.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(ConfigResources.GetIdentityResources())
+                .AddInMemoryApiResources(ConfigResources.GetApiResources())
+                .AddInMemoryClients(Clients.GetClients())
+                .AddTestUsers(Users.GetTestUsers())
+                .AddDeveloperSigningCredential()
+                ;
+            services.AddControllers();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseIdentityServer();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
